46c3d131149bf517b7d586dec26a89726e5e499b
J1vvoo <128969316+J1vvoo@users.noreply.github.com>
2024-09-08 02:28:45 +0900

    Update before_install.sh

--- script/before_install.sh
+++ script/before_install.sh
@@ -9,8 +9,12 @@ fi
 
 sudo pkill -f 'webgoat' || true
 
+echo "Checking if /app directory exists"
 if [ ! -d "/app" ]; then
   sudo mkdir /app
+  echo "/app directory created"
+else
+  echo "/app directory already exists"
 fi
 
 sudo chmod -R 755 /app
