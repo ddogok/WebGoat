7cb905b36cb3131671fac8e92ff6e5b81473bd13
ddogok <ddogok1210@gmail.com>
2024-09-09 01:20:21 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 # !/bin/bash
-sudo yum install -y java-17-amazon-corretto-devel
-BUILD_JAR=$(ls /home/ubuntu/build/*.jar)
+sudo yum install -y java-21-amazon-corretto-devel
+BUILD_JAR=$(ls /home/ec2-user/build/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/
+echo "> build 파일 복사" >> /home/ec2-user/deploy.log
+DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,8 +21,8 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
 # JARFILE=$(ls /home/ubuntu/build/target/*.jar)
 # echo "filename : $JARFILE" >> startsh.log
 
