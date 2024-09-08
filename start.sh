a781bc5dee958b37eb4b9124e2f32ff02b6a894c
ddogok <ddogok1210@gmail.com>
2024-09-08 21:25:13 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JARFILE=$(ls /home/ec2-user/build/target/*.jar)
+echo "filename : $JARFILE" >> startsh.log
+
+CURRENT_PID=$(pgrep -f $JARFILE)
+echo "currentpid : $CURRENT_PID" >> startsh.log
+kill -9 $CURRENT_PID 2>/dev/null
+sleep 5
+
+nohup java -jar $JARFILE > /dev/null 2> /dev/null < /dev/null &
